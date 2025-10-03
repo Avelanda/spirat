011e2a3363ad0e65b3ca4487d1f3653b93abeb0f
Avelanda <gorthell@gmail.com>
2025-10-03 10:33:01 +0200

    Update spirat.go
    Quantification of data processing as modular mechanisms for optimized granular objects.

--- spirat/spirat.go
+++ spirat/spirat.go
@@ -1,14 +1,48 @@
+/*
+ * Copyright © 2025 / Avelanda
+ * All rights reserved
+ */
+
+package main
 package spirat
 
 import "github.com/Hitachi/spirat/pkgmanager"
 
-type Spirat struct {
+func CoreSpirat(string, string){
+
+ type Spirat struct {
 	Command string    `json:"command"`
 	Version string    `json:"version"`
 	Results []*Result `json:"results"`
-}
+ }
 
-type Result struct {
+ type Result struct {
 	PackageManager string                  `json:"packageManager"`
 	QueryResult    *pkgmanager.QueryResult `json:"queryResult"`
+ }
+
+ for Spirat = string | int; Spirat == true; Spirat{
+    return
+ } 
+  if Spirat == false{
+    return Spirat == Spirat
+ }
+
+ for Result = string | int; Result == true; Result{
+    return
+ } 
+  if Result == false{
+    return Result == Result
+  }
+
+}
+
+func main(){
+  CoreSpirat := CoreSpirat
+  if CoreSpirat == CoreSpirat{
+    return  
+  }  
+   if !true || !false{
+    return CoreSpirat()
+   }
 }
